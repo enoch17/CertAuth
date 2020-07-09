687e8688fbaffe813057a69fbd966380420ec527
enoch17 <enochomolere1@gmail.com>
2020-07-09 18:35:20 +0000

    changes

--- testproject/src/server.js
+++ testproject/src/server.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-const bodyParser = require('body-parser');
-const express = require('express');
-
-const cors = require('cors');
-
-const app = express();
-=======
 
 const bodyParser = require('body-parser');
 const log = require('morgan')('dev');
@@ -13,7 +5,6 @@ const express = require('express');
 const cors = require('cors');
 
 
->>>>>>> 2d9a6844b4b1d655c73661a9429896cd2a1d6bbc
 app.use(bodyParser.urlencoded({extended: true}));
 // Configure bodyparser
 const bodyParserJSON = bodyParser.json();
@@ -34,21 +25,12 @@ const router = require("./route");
 app.use("/api/v1", router);
 
 
-<<<<<<< HEAD
-app.get('/api/v1/w', (req, res) =>
-	res.json({ message: 'Hello World! Welcome' })
-=======
 app.get('/api/v1', (req, res) =>
 	res.json({ message: 'Hello World! Welcome to iDeyPay Till Engine' })
->>>>>>> 2d9a6844b4b1d655c73661a9429896cd2a1d6bbc
 );
 
 const PORT = 3000;
 app.listen(PORT, () => {
 	
 	console.log(`server started on port ${PORT}`);
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 2d9a6844b4b1d655c73661a9429896cd2a1d6bbc
